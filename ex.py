0317b94a4bc4a60a4de485b9b1bc1dbf88b054a4
97danielj <97danielj@google.com>
2022-09-12 23:37:13 +0900

    Upload Naver Crawling ipynb 220912

--- ex.py
+++ ex.py
@@ -1,11 +1,18 @@
 import json
-with open('./Crawling2/naver_서울 횟집_review_dic_page6.json','r', encoding ='utf-8') as f:
-    review_dic = json.load(f)
+key_word_list = ['서울 횟집','인천 횟집', '부산 횟집', '대구 횟집', '광주 횟집', '대전 횟집', '울산 횟집']
+#page_num_ex = 3
 
-
-print(review_dic.keys())
-print(len(review_dic))
-print(review_dic['한영상회'])
-
-
+for page_num in range(1,7):
+    for key_word in key_word_list:
+        print(f'key_word : {key_word}, page_num : {page_num}')
+        with open(f'naver/naver_{key_word}_review_dic_page{page_num}.json', 'r', encoding ='utf-8') as f:
+            review_dic = json.load(f)
+        print(len(review_dic))
+'''
+for key_word in key_word_list:
+    with open(f'./naver/naver_{key_word}_review_dic_page{page_num_ex}.json', 'r', encoding='utf-8') as f:
+        review_dic = json.load(f)
+    print(len(review_dic))
+    print(review_dic.keys())
+'''
 
--- scrollbody.py
+++ scrollbody.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
-from bs4 import BeautifulSoup
 import re
 import json
 WAIT = 3
