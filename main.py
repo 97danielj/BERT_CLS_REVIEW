d5814a11f80e7e534931852abc651e61df1529bd
DanielJeong <97danielj@gmail.com>
2022-10-10 19:54:16 +0900

    modify crawling2.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #options = webdriver.ChromeOptions()
 #options.add_argument('headless')
 
-from crawling import *
+import json
+from selenium import webdriver
+from crawling2 import *
 from find_element import *
 from scrollbody import *
 
@@ -15,7 +17,7 @@ def naver_crawling(key_word, page_num, ck_pt_idx = 0):
     # 매장 정보를 저장할 딕셔너리 구조체
     review_dic = dict()
     if ck_pt_idx != 0:
-        with open(f'./naver/naver_{key_word}_review_dic_page{page_num}.json', 'r', encoding='utf-8') as f:
+        with open(f'./naver_review_with_rank/naver_{key_word}_review_dic_page{page_num}.json', 'r', encoding='utf-8') as f:
             review_dic = json.load(f)
 
     page_num = page_num - 1
@@ -77,7 +79,7 @@ def naver_crawling(key_word, page_num, ck_pt_idx = 0):
             # 예상치 못한 오류가 뜨는 경우
             except:
                 # json파일 저장
-                with open(f'./naver/naver_{key_word}_review_dic_page{page_num + 1}.json', 'w', encoding='utf-8') as f:
+                with open(f'./naver_review_with_rank/naver_{key_word}_review_dic_page{page_num + 1}.json', 'w', encoding='utf-8') as f:
                     json.dump(review_dic, f, indent=4, ensure_ascii=False)
                 #현재 driver는 닫기
                 driver.close()
@@ -112,7 +114,7 @@ def naver_crawling(key_word, page_num, ck_pt_idx = 0):
 
     print('크롤링을 저장하고, 종료합니다.')
     # json 파일로 저장
-    with open(f'./naver/naver_{key_word}_review_dic_page{page_num + 1}.json', 'w', encoding='utf-8') as f:
+    with open(f'./naver_review_with_rank/naver_{key_word}_review_dic_page{page_num + 1}.json', 'w', encoding='utf-8') as f:
         json.dump(review_dic, f, indent=4, ensure_ascii=False)
 
     driver.close()
@@ -120,12 +122,11 @@ def naver_crawling(key_word, page_num, ck_pt_idx = 0):
     return
 
 key_word_list = ['서울 횟집','인천 횟집', '부산 횟집', '대구 횟집', '광주 횟집', '대전 횟집', '울산 횟집']
-#key_word_list = ['부산 횟집', '대구 횟집', '광주 횟집', '대전 횟집', '울산 횟집']
-'''for page_num in range(4,7):
+
+for page_num in range(1,7):
     for key_word in key_word_list:
         naver_crawling(key_word, page_num)
-'''
-naver_crawling(key_word_list[-1], 3)
+
 
 
 
--- scrollbody.py
+++ scrollbody.py
@@ -1,12 +1,7 @@
-import time
-from selenium import webdriver
+
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
-import re
-import json
+
 WAIT = 3
 SCROLL_PAUSE_SEC = 1
 #options = webdriver.ChromeOptions()
